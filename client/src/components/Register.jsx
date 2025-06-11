e3d097e54c3286589c74d50535030036a98762d2
Passio <passiotti.agustin@gmail.com>
2025-06-11 15:52:04 -0300

    register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Register.css'
 
 function Register() {
 return (
-    <div>Register</div>
+<div className="register-container">
+  <h2 className='register-title'>Register</h2>
+  <form>
+    <div className="register-form">
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" name="username" required />
+    
+    
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" required />
+    
+    
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" required />
+    </div>
+    <button className='register-button' type="submit">Register</button>
+  </form>
+</div>
 )
 }
 
